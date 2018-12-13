393ea4d9e5c0bde40b4b4cd9cbd678bdfc44ca66
Open Close <interfiancee@gmail.com>
2018-12-13 09:42:40 +0800

    removed useless ModList

--- scripts/main.js
+++ scripts/main.js
@@ -7,8 +7,7 @@ const UNI_HEADER = "Partner University";
 let rawTa = document.getElementById("raw");
 let modTa = document.getElementById("mod");
 
-
-function parseRaw() {
+function parseRaw () {
 	//first things first get raw data
 	let raw = rawTa.value;
 	let colHeaders = (raw.split("\n", 1)[0]).split(DELIM_HEADER);
@@ -41,13 +40,12 @@ function parseRaw() {
 		crUni = curRow[uniIndex];
 		crNMod = curRow[nModIndex];
 
-		for (let j = 0; j < mods.length; j++) {
-			
+		for (const mod of mods) {
 
-			if (mods[j] === crNMod) {
+			if (mod === crNMod) {
 
 				if (schDict[crUni] === undefined) {
-					schDict[crUni] = new ModList();
+					schDict[crUni] = new Set();
 				}
 
 				schDict[crUni].add(crNMod);
@@ -55,16 +53,12 @@ function parseRaw() {
 		}
 	}
 
-
 	let outputTa = document.getElementById("output");
 	let outStr = "";
 
-	for (x of Object.keys(schDict)) {
-
-		let uni = x;
-		let nMods = schDict[x].toString("\n\t");
-
-		outStr += uni + "" + nMods;
+	for (const [uni, unsortedModules] of Object.entries(schDict)) {
+		const sortedModules = Array.from(unsortedModules).sort();
+		outStr += [uni, ...sortedModules].join("\n\t");
 		outStr += "\n\n\n";
 	}
 
@@ -72,57 +66,6 @@ function parseRaw() {
 
 	//scroll down
 	window.scrollBy(0, 1000);
-}
-
-//implementation of ordered list of strings with no duplicates
-class ModList {
-
-	constructor () {
-		
-		this.xs = [];
-	}
-
-	//does a binary search of the array to see if the element already exists.
-	//if it doesn't then adds the element to the appropriate position.
-	add(m) {
-
-		let thisXs = this.xs;
-
-		function helper(low, high) {
-
-			let mid = Math.floor((low + high) / 2);
-
-			if (thisXs[mid] === m)	{	//a copy of m has already been found in the array
-				return;
-			}
-			else if (low === high) {	//m is confirmed to not exist in the array
-
-				if (thisXs[low] < m)
-					low++;
-				
-				thisXs.splice(low, 0, m);
-			}
-			else if (low === mid) 
-				helper(low, low);
-			else if (thisXs[mid] > m)
-				helper(low, mid);
-			else
-				helper(mid, high);
-		}
-
-		helper(0, thisXs.length);
-	}
-
-	toString(separator) {
-
-		let rv = "";
-
-		this.xs.forEach(element => {
-			rv += separator + element;
-		});
-
-		return rv;
-	}
 }
 
 document.getElementById("ok").addEventListener("click", parseRaw);
